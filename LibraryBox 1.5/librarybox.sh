626e259bfe3543cd9f7cd881c462344cc088196b
Griffey <griffey@gmail.com>
2012-08-30 13:44:52 -0500

    Installation scripts! instructions soon

---
+++ LibraryBox 1.5/librarybox.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+echo "Press return, or enter custom path to /librarybox"
+read usrpath
+
+if [ "$usrpath" = "" ]
+then
+  path="/mnt/usb/librarybox"
+else
+  path="$usrpath"
+fi
+echo "Copying from $path"
+echo "Making your LibraryBox..."
+
+cp $path/droopy /opt/piratebox/bin/;
+cp $path/hosts /opt/piratebox/conf/;
+cp $path/index.html /opt/piratebox/chat/;
+cp $path/piratebox-logo-small.png /opt/piratebox/src/;
+cp $path/piratebox-logo.png /opt/piratebox/src/;
+cp $path/piratebox.conf /opt/piratebox/conf/;
+cp $path/wireless /etc/config/wireless/;
+
+cp $path/READ.ME.htm /opt/piratebox/src/; 
+mv /opt/piratebox/src/READ.ME.htm /opt/piratebox/src/.READ.ME.htm;
+
+echo "Done! Enjoy your LibraryBox!"
