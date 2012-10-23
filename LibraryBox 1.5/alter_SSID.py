30ccef784790d72fb763ce0fe01ce0c02896bb1c
Andromeda Yelton <andromeda.yelton@gmail.com>
2012-10-22 22:06:33 -0400

    allow user to set command-line option for SSID

--- LibraryBox 1.5/alter_SSID.py
+++ LibraryBox 1.5/alter_SSID.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
 from os import remove, rename
+from sys import argv
 
 def alter_SSID():
     """
     customize the SSID for Librarybox
+    if a command-line argument is taken, it will use that as the SSID
+    otherwise it will default to Librarybox - Free Content
     """
     source = open('/etc/config/wireless', 'r')
     destination = open('/etc/config/wireless_new', 'w')
 	
     for line in source:
         if (line.find('PirateBox - Share Freely') > -1):
-            line = line.replace('PirateBox - Share Freely', 'LibraryBox - Free Content!')
+            if argv[1]:
+                line = line.replace('PirateBox - Share Freely', argv[1])
+            else:
+                line = line.replace('PirateBox - Share Freely', 'LibraryBox - Free Content!')
         destination.write(line)
 
     source.close()
